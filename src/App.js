1738fde57fbb65ae07340e7bf9dc6a82ec63a0a6
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-01-20 21:01:52 -0300

    version estable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
   return (
     <div className="App container">
       <h1>Desafío Estado de los Componentes y Eventos</h1>
-      <Button text = "Iniciar sesión"></Button>
+      <form>
+        <label>Nombre</label>
+        <input className='form-control'></input>
+        <label>Password</label>
+        <input className='form-control' type='password'></input>
+      </form>
+      <div className='mt-3'>
+        <Button text = "Iniciar sesión"></Button>
+      </div>      
     </div>
   );
 }
