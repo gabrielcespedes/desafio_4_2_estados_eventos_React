48756fad3f04562b2ced1afd1898ed5fe668c323
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-01-20 22:40:52 -0300

    version estable

--- src/App.js
+++ src/App.js
@@ -6,42 +6,33 @@ import Btn from './components/Button';
 
 
 function App() {
-  const [buttonActivated, setButtonActivated] = useState(false);
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
-    
-
-  const evalContent = () => {
-    if (name.length > 0 && password.length > 0) {
-      setButtonActivated(true);
-    } else {
-      setButtonActivated(false);
-    }
-  }
+  const [display, setDisplay] = useState(false);
+  const [correct, setCorrect] = useState(false);  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("evaluar nombre y password");
     const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
+    setDisplay(true);
     if (name === 'ADL' && password === "252525") {
-      alert('Bienvenido');
+      setCorrect(true);
     } else {
-      alert('datos incorrectos');
+      setCorrect(false);
     }
   }
 
   return (
     <div className="App container w-50">
       <h1 className='text-center'>Desafío Estado de los Componentes y Eventos</h1>
-      <div className='alert alert-danger'>Datos incorrectos</div>
+      {display && correct && <div className='alert alert-success'>Datos correctos</div>}   
+      {display && !correct && <div className='alert alert-danger'>Datos incorrectos</div>}   
       <form onSubmit={handleSubmit}>
         <label>Nombre</label>
-        <input id='name' className='form-control' type='text' onChange={(e) => {setName(e.target.value)
-        evalContent()}}></input>
+        <input id='name' className='form-control' type='text' onChange={(e) => {setName(e.target.value)}}></input>
         <label>Password</label>
-        <input id='password' className='form-control' type='password' onChange={(e) => {setPassword(e.target.value)
-        evalContent()}}></input>      
+        <input id='password' className='form-control' type='password' onChange={(e) => {setPassword(e.target.value)}}></input>      
         <div className='mt-3'>
           {name.length > 0 && password.length >0 && <Btn text = "Iniciar sesión"></Btn>}        
         </div>  
